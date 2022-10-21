187dca1dd2a39a02468230cfa08da91181842277
mbartos00 <maciekbartos8@gmail.com>
2022-10-21 18:40:59 +0200

    add btns, lightbox, animation

--- lab2/index.js
+++ lab2/index.js
@@ -1,63 +1,73 @@
 const slides = document.querySelectorAll('.slide');
-const nextBtn = document.querySelector('.next')
-const prevBtn = document.querySelector('.prev')
-
+const slideItem = document.querySelectorAll('.slide-item');
+const slideshowContainer = document.querySelector('.slideshow-container');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const dots = document.getElementsByClassName('dot');
+const stopBtn = document.querySelector('.stop-btn');
+const startBtn = document.querySelector('.start-btn');
+const closeBtn = document.querySelector('.close-btn');
 let slideIndex = 1;
+const isVisible = (item) => item.className !== 'hidden';
 
-const isVisible = (item) => item.className !== "hidden";
-
-setInterval(() =>{
-    for(let i =0; i< slides.length; i++) {
-     
-            if(isVisible(slides[i]) && i === slideIndex){
-                slides[i].classList.remove("hidden")
-            }
-            else{
-                slides[i].classList.add('hidden')
-
-            }
-
-      
+const startSlider = () => {
+  for (let i = 0; i < slides.length; i++) {
+    if (isVisible(slides[i]) && i === slideIndex) {
+      slides[i].classList.remove('hidden');
+      dots[i].classList.add('active');
+    } else {
+      slides[i].classList.add('hidden');
+      dots[i].classList.remove('active');
     }
-    if(slideIndex == slides.length){
-        slideIndex = 0
-        slides[slideIndex].classList.remove("hidden")
-    }
-    slideIndex++    
-},2000)
+  }
+  if (slideIndex == slides.length) {
+    slideIndex = 0;
+    slides[slideIndex].classList.remove('hidden');
+    dots[slideIndex].classList.add('active');
+  }
 
-    
+  slideIndex++;
+};
+const interval = setInterval(startSlider, 3000);
 
-nextBtn.addEventListener('click', () =>{
+slideItem.forEach((item) => {
+  if (item.getAttribute('src')) {
+    item.addEventListener('click', () => {
+      slideshowContainer.classList.add('shown');
+      closeBtn.classList.add('shown');
+      clearInterval(interval);
+    });
+  }
+});
+closeBtn.addEventListener('click', () => {
+  slideshowContainer.classList.remove('shown');
+  setInterval(startSlider, 3000);
+  closeBtn.classList.remove('shown');
+});
+stopBtn.addEventListener('click', () => clearInterval(interval));
+startBtn.addEventListener('click', () => setInterval(startSlider, 3000));
 
-    for(let i =0; i< slides.length; i++) {
-        
-        if(isVisible(slides[i]) && i === slideIndex){
-            slides[i].classList.remove("hidden")
-        }
-        else{
-            slides[i].classList.add('hidden')
-
-        }
+nextBtn.addEventListener('click', () => {
+  for (let i = 0; i < slides.length; i++) {
+    if (isVisible(slides[i]) && i === slideIndex) {
+      slides[i].classList.remove('hidden');
+    } else {
+      slides[i].classList.add('hidden');
     }
-    if(slideIndex == slides.length){
-        slideIndex = 0
-        slides[slideIndex].classList.remove("hidden")
+  }
+  if (slideIndex == slides.length) {
+    slideIndex = 0;
+    slides[slideIndex].classList.remove('hidden');
+  }
+  slideIndex++;
+});
+prevBtn.addEventListener('click', () => {
+  for (let i = 0; i < slides.length; i++) {
+    if (isVisible(slides[i]) && i === slideIndex) {
+      slides[i].classList.remove('hidden');
+    } else {
+      slides[i].classList.add('hidden');
     }
-    slideIndex++
-    
-})
-prevBtn.addEventListener('click', () =>{
-    for(let i =0; i < slides.length; i++) {
-        
-        if(isVisible(slides[i]) && i === slideIndex){
-            slides[i].classList.remove("hidden")
-        }
-        else{
-            slides[i].classList.add('hidden')
-            
-        }
-    }
-    slideIndex--
- 
-})
+  }
+  slideIndex--;
+});
